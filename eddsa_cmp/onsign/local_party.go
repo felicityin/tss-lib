faea21cdcd33a6d5b6e5ad586b826c4d8145c1e1
felicityin <yinjingping2022@gmail.com>
2024-04-10 12:05:01 +0800

    chore: modify readme

--- eddsa_cmp/onsign/local_party.go
+++ eddsa_cmp/onsign/local_party.go
@@ -145,7 +145,7 @@ func (p *LocalParty) StoreMessage(msg tss.ParsedMessage) (bool, *tss.Error) {
 		p.temp.signRound3Messages[fromPIdx] = msg
 
 	default: // unrecognised message, just ignore!
-		common.Logger.Warningf("unrecognised message ignored: %v", msg)
+		common.Logger.Warnf("unrecognised message ignored: %v", msg)
 		return false, nil
 	}
 	return true, nil
